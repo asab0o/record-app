a82ddd112e60861561b5b09ef6b8e39877240899
0512br <asa.pf1205@gmail.com>
2021-11-28 13:40:14 +0900

    install laravel breeze

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Record</title>
-</head>
-<body>
-  <div class="list">テスト</div>
-</body>
-</html>
+@extends('layouts.parent')
+@section('content')
+
+{{-- <form action="{{ route() }}" method="post"> --}}
+{{ csrf_field() }}
+<ul>
+  <li>あ</li>
+  <li>い</li>
+  <li>う</li>
+</ul>
+
+
+
+</form>
